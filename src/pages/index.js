b00edc877165884c2ac282de4afa475fb21c304b
Oleg <20502299+olegkovalenkome@users.noreply.github.com>
2018-09-20 10:44:57 +0300

    Hero styles & Images had been added

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,13 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Learn design and code React apps</h1>
-    <p>Complete courses about the best tools for designers</p>
-    <Link to="/page-2/">Watch video</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn design and code React apps</h1>
+        <p>Complete courses about the best tools for designers</p>
+        <Link to="/page-2/">Watch this video</Link>
+      </div>
+    </div>
   </Layout>
 )
 
