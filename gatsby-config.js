9588ff708f0e6f7f31b9881cab193d3c8bacf632
Oleg <20502299+olegkovalenkome@users.noreply.github.com>
2018-09-22 11:18:00 +0300

    End Hero section animations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Design+Code 3',
+    title: 'Gatsby Default Starter',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Learn design and code React apps</h1>
+        <h1>Learn to<br />design and code React apps</h1>
         <p>Complete courses about the best tools for designers</p>
         <Link to="/page-2/">Watch this video</Link>
       </div>
